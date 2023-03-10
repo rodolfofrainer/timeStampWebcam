1d8d3545f8a7e57c3e482d5478ab63687dd03416
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2023-03-10 13:38:40 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import cv2
+import streamlit as st
+import time
+import datetime
+
+st.title('Motion Detector')
+start = st.button('Start Camera')
+
+time.sleep(1)
+if start:
+    streamlit_image = st.image([])
+    camera = cv2.VideoCapture(0)
+
+    while True:
+        now = datetime.datetime.now()
+        check, frame = camera.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        cv2.putText(img=frame, text=f'{now.strftime("%A")}', org=(50, 50), fontFace=cv2.FONT_HERSHEY_PLAIN,
+                    fontScale=2, color=(0, 255, 255), thickness=2, lineType=cv2.LINE_AA)
+
+        cv2.putText(img=frame, text=f'{now.strftime("%H:%M:%S")}', org=(25, 100), fontFace=cv2.FONT_HERSHEY_PLAIN,
+                    fontScale=2, color=(255, 0, 0), thickness=2, lineType=cv2.LINE_AA)
+
+        streamlit_image.image(frame)
